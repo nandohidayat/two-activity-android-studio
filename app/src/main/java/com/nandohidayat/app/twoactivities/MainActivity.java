90fbd16020b15559da567c2f2aabdeb02f43600b
Nando <mnandohidayat@gmail.com>
2018-09-27 21:28:42 +0700

    add editText

--- app/src/main/java/com/nandohidayat/app/twoactivities/MainActivity.java
+++ app/src/main/java/com/nandohidayat/app/twoactivities/MainActivity.java
@@ -5,18 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.nandohidayat.app.twoactivities.extra.MESSAGE";
+    private EditText mMassageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMassageEditText = (EditText) findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMassageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
